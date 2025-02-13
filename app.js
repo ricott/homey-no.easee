2e250f0094ad8c47a85fac4b1baafb97d1517158
Richard Barkestam <richard@barkestam.se>
2025-02-13 22:13:28 +0100

    Maintenance: Update dependencies and migrate from http.min to node-fetch

--- app.js
+++ app.js
@@ -4,20 +4,36 @@ const { App } = require('homey');
 const Stats = require('./lib/stats.js');
 
 class EaseeApp extends App {
-	async onInit() {
-		this.log(`Easee Home v${this.getAppVersion()} is running`);
-        this.stats = new Stats();
+    async onInit() {
+        this.log(`Easee Home v${this.getAppVersion()} is running`);
+        this.setupGlobalFetch();
 
+        this.stats = new Stats();
         this.homey.setInterval(() => {
             this.#printStatsToSettings();
         }, 60 * 1000 * 5);
-	}
+    }
+
+    setupGlobalFetch() {
+        if (!global.fetch) {
+            global.fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+        }
+        if (!global.AbortSignal.timeout) {
+            global.AbortSignal.timeout = timeout => {
+                const controller = new AbortController();
+                const abort = setTimeout(() => {
+                    controller.abort();
+                }, timeout);
+                return controller.signal;
+            }
+        }
+    }
 
     #printStatsToSettings() {
         this.homey.settings.set('startOfStats', this.getStats().getInitDatetime(this.homey.clock.getTimezone()));
         this.homey.settings.set('totalInvocations', this.getStats().getTotalInvocations());
         this.homey.settings.set('invocations', util.inspect(this.getStats().getInvocationStats(), { showHidden: false, depth: null }));
-        this.homey.settings.set('errors', util.inspect(this.getStats().getErrorList(), { showHidden: false, depth: null }));     
+        this.homey.settings.set('errors', util.inspect(this.getStats().getErrorList(), { showHidden: false, depth: null }));
     }
 
     getStats() {
